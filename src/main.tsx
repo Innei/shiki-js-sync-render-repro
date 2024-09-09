ff474c5be594db7f984b4f377b4c4b016fabf701
Innei <i@innei.in>
2024-09-09 23:07:11 +0800

    update
    Signed-off-by: Innei <i@innei.in>

--- src/main.tsx
+++ src/main.tsx
@@ -2,38 +2,89 @@ import {
   createHighlighterCoreSync,
   createJavaScriptRegexEngine,
 } from 'shiki/index.mjs'
+import c from 'shiki/langs/c.mjs'
 import cpp from 'shiki/langs/cpp.mjs'
 import nord from 'shiki/themes/nord.mjs'
 
-const time = Date.now()
-const js = createJavaScriptRegexEngine()
-const shiki = createHighlighterCoreSync({
-  themes: [nord],
-  langs: [cpp],
-  engine: js,
-})
-const code = `...
+setTimeout(() => {
+  const time = Date.now()
+  const js = createJavaScriptRegexEngine()
+  const shiki = createHighlighterCoreSync({
+    themes: [nord],
+    langs: [cpp, c],
+    engine: js,
+  })
+  const code = `static int binder_translate_binder(struct flat_binder_object *fp,  
+  
+                                     struct binder_transaction *t,  
+  
+                                     struct binder_thread *thread)  
+  
+  {  
+  
+      struct binder_node *node;  
+  
+      struct binder_proc *proc = thread->proc;  
+  
+      struct binder_proc *target_proc = t->to_proc;  
+  
+      struct binder_ref_data rdata;  
+  
+      int ret = 0;  
+  
+    
+  
+      node = binder_get_node(proc, fp->binder);  
+  
+      if (!node) {  
+  
+          node = binder_new_node(proc, fp);  
+  
+          if (!node)  
+  
+              return -ENOMEM;  
+  
+      }  
+  
+      ...  
+  
+      ret = binder_inc_ref_for_node(target_proc, node,  
+  
+                                    fp->hdr.type == BINDER_TYPE_BINDER,  
+  
+                                    &thread->todo, &rdata);  
+  
+      ...  
+  
+      if (fp->hdr.type == BINDER_TYPE_BINDER)  
+  
+          fp->hdr.type = BINDER_TYPE_HANDLE;  
+  
+      else  
+  
+          fp->hdr.type = BINDER_TYPE_WEAK_HANDLE;  
+  
+      fp->binder = 0;  
+  
+      fp->handle = rdata.desc;  
+  
+      fp->cookie = 0;  
+  
+    
+  
+      ...  
+  
+      done:  
+  
+      binder_put_node(node);  
+  
+      return ret;  
+  
+  }`
+  shiki.codeToHtml(code, {
+    lang: 'c',
+    theme: 'nord',
+  })
 
-pid_t pid = zygote::ForkCommon(env, /* is_system_server= */ false, fds_to_close, fds_to_ignore,  true);
-
-if (pid == 0) {  
-
-    SpecializeCommon(env, uid, gid, gids, runtime_flags, rlimits, capabilities, capabilities,  
-
-                     mount_external, se_info, nice_name, false, is_child_zygote == JNI_TRUE,  
-
-                     instruction_set, app_data_dir, is_top_app == JNI_TRUE, pkg_data_info_list,  
-
-                     allowlisted_data_info_list, mount_data_dirs == JNI_TRUE, 
-
-                     mount_storage_dirs == JNI_TRUE);  
-
-}
-
-return pid;`
-shiki.codeToHtml(code, {
-  lang: 'c++',
-  theme: 'nord',
-})
-
-console.log('Time:', `${Date.now() - time}ms`)
+  alert('Render time:' + `${Date.now() - time}ms`)
+}, 300)
